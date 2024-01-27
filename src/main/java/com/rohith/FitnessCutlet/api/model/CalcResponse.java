4e3806943e7cc61361bd3b0fa978fa9ea9099823
Rohith Chevvakula <chevvakula.r@northeastern.edu>
2024-01-27 02:19:33 -0500

    Code refactor: introducing lombok; upgrading the dependency version

--- src/main/java/com/rohith/FitnessCutlet/api/model/CalcResponse.java
+++ src/main/java/com/rohith/FitnessCutlet/api/model/CalcResponse.java
@@ -1,13 +1,8 @@
 package com.rohith.FitnessCutlet.api.model;
 
+import lombok.Data;
 
+@Data
 public class CalcResponse {
     private double amoutOfCalories;
-
-    public double getAmoutOfCalories() {
-        return amoutOfCalories;
-    }
-    public void setAmoutOfCalories(double amoutOfCalories) {
-        this.amoutOfCalories = amoutOfCalories;
-    }
 }
--- src/main/java/com/rohith/FitnessCutlet/api/model/UserData.java
+++ src/main/java/com/rohith/FitnessCutlet/api/model/UserData.java
@@ -1,60 +1,22 @@
 package com.rohith.FitnessCutlet.api.model;
 
+import lombok.Data;
 import org.springframework.stereotype.Component;
 
 @Component
+@Data
 public class UserData {
-    String fitnessLevel,age, height, weight, gender;
-
-    public String getGender() {
-        return gender;
-    }
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-    public int getAge() {
-        return Integer.parseInt(age);
-    }
-    public void setAge(String age) {
-        this.age = age;
-    }
-
-    public int getHeight() {
-        return Integer.parseInt(height);
-    }
-    public void setHeight(String height) {
-        this.height = height;
-    }
-
-    public int getWeight() {
-        return Integer.parseInt(weight);
-    }
-    public void setWeight(String weight) {
-        this.weight = weight;
-    }
+    String fitnessLevel, gender;
+    double age, height, weight;
 
     public double getFitnessLevel() {
-        if(fitnessLevel.equals("sedentary")){
-            return 1.2;
-        }
-        else if(fitnessLevel.equals("lightly-acitve")){
-            return 1.375;
-        }
-        else if(fitnessLevel.equals("moderately-active")){
-            return 1.55;
-        }
-        else if(fitnessLevel.equals("very-active")){
-            return 1.725;
-        }
-        else if(fitnessLevel.equals("extremly-active")){
-            return 1.9;
-        }
-        // else { // throw exception
-        //  }
-        return 0;
-    }
-
-    public void setFitnessLevel(String fitnessLevel) {
-        this.fitnessLevel = fitnessLevel;
+        return switch (fitnessLevel) {
+            case "sedentary" -> 1.2;
+            case "lightly-acitve" -> 1.375;
+            case "moderately-active" -> 1.55;
+            case "very-active" -> 1.725;
+            case "extremly-active" -> 1.9;
+            default -> 0;
+        };
     }
 }
